d8d73fa9c6a222462659106bee8259a6b18679eb
rostomer <rostomerr@gmail.com>
2019-03-04 15:30:37 +0300

    minor prefabs changes

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,8 @@ public class SpawnManager : MonoBehaviour {
 
     [SerializeField]
     private GameObject _EnemyPrefab;
+    [SerializeField]
+    private GameObject _PlayerPrefab;
 
     [SerializeField]
     private GameObject[] _powerUps;
@@ -15,9 +17,13 @@ public class SpawnManager : MonoBehaviour {
     private float _PowerUpSpawnRate = 20f;
     private float _canSpawnEnemy = 3;
 
+    private void OnEnable()
+    {
+        Instantiate(_PlayerPrefab, Vector3.zero, Quaternion.identity);
+    }
+
     void Update()
     {
-       
             if (Time.time > _canSpawnEnemy)
             {
                 Instantiate(_EnemyPrefab, transform.position, Quaternion.identity);
@@ -26,12 +32,12 @@ public class SpawnManager : MonoBehaviour {
             }
 
             if(Time.time > _canSpawnPowerUp)
-        {
+            {
             int choice = Random.Range(0, _powerUps.Length);
 
             Instantiate(_powerUps[choice], new Vector3(Random.Range(-7f, 7f), 9, 0), Quaternion.identity);
 
             _canSpawnPowerUp = Time.time + _PowerUpSpawnRate;
-        }       
+            }       
     }
 }
--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -1,16 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour {
 
+    [SerializeField]
+    private Image livesImage;
+    [SerializeField]
+    private Text _scoreText;
+    [SerializeField]
+    private Sprite[] _liveSprites;
+
+    [SerializeField]
+    private GameObject[] _unactiveObjects;
+
+    private bool _objectsAreActive = false;
+
+    public int CurrentScore { get; set; }
+
+    //   private GameObject player;
+
+    public void UpdateLives(int lives)
+    {
+        Debug.Log("Player lives: " + lives);
+            livesImage.sprite = _liveSprites[lives];
+    }
+
+    public void UpdateScore(int scoreValueToAdd)
+    {
+        CurrentScore += scoreValueToAdd;
+        _scoreText.text = "Score: " + CurrentScore;
+    }
 	// Use this for initialization
 	void Start () {
-		
+      
 	}
 	
+    public void ActivateMenu()
+    {
+            foreach (GameObject obj in _unactiveObjects)
+            {
+                obj.SetActive(_objectsAreActive);
+            }
+            _objectsAreActive = !_objectsAreActive;
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (gameObject.activeSelf && Input.GetKeyDown(KeyCode.Space) && !_objectsAreActive)
+        {
+            ActivateMenu();
+        }
+
+    }
 }
